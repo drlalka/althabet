678423c8133cc809198b40ff5a7451e50127021e
Turovceva Olya <telvanni.wizard42@gmail.com>
2021-10-18 19:56:56 +0300

    create W

--- printer/main.cpp
+++ printer/main.cpp
@@ -110,7 +110,14 @@ array<string, 6> v()
 }
 array<string, 6> w()
 {
-
+return bukv;array<string, 6> bukv;
+    bukv[0]="||           || ";
+    bukv[1]="||    ||     || ";
+    bukv[2]=" ||  || ||  ||  ";
+    bukv[3]=" ||  || ||  ||  ";
+    bukv[4]="  ||||   ||||   ";
+    bukv[5]="   ||     ||    ";
+return bukv;
 }
 array<string, 6> x()
 {
